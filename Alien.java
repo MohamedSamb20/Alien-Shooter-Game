22674f473fd3cd5ae7fc53cd9f11c676e56048c9
Mohamed Samb <46804837+MohamedSamb20@users.noreply.github.com>
2020-07-18 01:27:10 -0700

    Create Alien.java

---
+++ Alien.java
@@ -0,0 +1,18 @@
+public class Alien
+{
+    // instance variables - replace the example below with your own
+    int x;
+    int y;
+    int h;
+    int w;
+
+ 
+    public Alien(int x, int y, int h, int w)
+    {
+        this.x = x;
+        this.y = y;
+        this.h = h;
+        this.w = w;
+    }
+
+}
