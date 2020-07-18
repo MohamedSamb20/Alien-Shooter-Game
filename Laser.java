95597751151e139f7ef3b293b8bf2fb618f22963
Mohamed Samb <46804837+MohamedSamb20@users.noreply.github.com>
2020-07-18 01:30:07 -0700

    Create Laser.java

---
+++ Laser.java
@@ -0,0 +1,18 @@
+public class Laser
+{
+    // instance variables - replace the example below with your own
+    boolean isFired;
+    boolean isVisible;
+    int x;
+    int y;
+
+ 
+    public Laser(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        isFired = false;
+        isVisible = false;
+    }
+
+}
