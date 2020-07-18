a34f4b040f1ab0741e7b97f50414507037ba890e
Mohamed Samb <46804837+MohamedSamb20@users.noreply.github.com>
2020-07-18 01:30:38 -0700

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,18 @@
+public class Player
+{
+    // instance variables - replace the example below with your own
+    int x;
+    int y;
+    int h;
+    int w;
+
+ 
+    public Player(int x, int y, int h, int w)
+    {
+        this.x = x;
+        this.y = y;
+        this.h = h;
+        this.w = w;
+    }
+
+}
