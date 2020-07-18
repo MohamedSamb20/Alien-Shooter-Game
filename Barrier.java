6e9ff6a8e3b60cfe34c076e8198d17fbf13cf0bb
Mohamed Samb <46804837+MohamedSamb20@users.noreply.github.com>
2020-07-18 01:27:36 -0700

    Create Barrier.java

---
+++ Barrier.java
@@ -0,0 +1,18 @@
+public class Barrier
+{
+    int x;
+    int y;
+    int h;
+    int w;
+
+ 
+    public Barrier(int x, int y, int h, int w)
+    {
+        this.x = x;
+        this.y = y;
+        this.h = h;
+        this.w = w;
+    }
+
+
+}
